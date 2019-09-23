bfc1105c23520b3358ef32b2be35c39991b053e6
plhewa <plhewa2@gmail.com>
2019-09-23 17:38:37 +0530

    Trip booking details

--- app/src/main/java/com/example/urban_crew_extended/BookNowAltoK10.java
+++ app/src/main/java/com/example/urban_crew_extended/BookNowAltoK10.java
@@ -4,12 +4,17 @@ import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.TimePickerDialog;
 import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog.Builder;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -48,6 +53,7 @@ public class BookNowAltoK10 extends AppCompatActivity{
     String amPm;
     FirebaseAuth firebaseAuth;
     FirebaseDatabase firebaseDatabase;
+    private EditText tripLocation, tripPickup, tripDate, tripTime, tripBook, tripExtra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,30 +81,14 @@ public class BookNowAltoK10 extends AppCompatActivity{
                     Toast.makeText(BookNowAltoK10.this, "Field's can't be empty", Toast.LENGTH_LONG).show();
                     return;
                 }
-                YourLocation = userLocation_1.getText().toString();
-                PickUpLocation = userpickupLocation.getText().toString();
-                Date = editText_date.getText().toString();
-                Time = editText_time.getText().toString();
-
-                if (YourLocation.isEmpty() | PickUpLocation.isEmpty() | Date.isEmpty() | Time.isEmpty()){
-
-                    Toast.makeText(BookNowAltoK10.this, "Field's can't be empty", Toast.LENGTH_LONG).show();
-                    return;
-                }
 
                 SendInput();
-                openSuccess();
-            }
-        });
+                openDialog();
 
-                SendInput();
-                Intent intent = new Intent(BookNowAltoK10.this, PaymentSelection.class);
-                startActivity(intent);
             }
         });
 
 
-        editText_date = findViewById(R.id.datePicker_alto);
         editText_date = findViewById(R.id.datePicker_alto);
         final Calendar calendar = Calendar.getInstance();
         editText_date.setOnClickListener(new OnClickListener() {
@@ -186,99 +176,111 @@ public class BookNowAltoK10 extends AppCompatActivity{
         });
     }
 
-
-
-
-
-        editText_time = findViewById(R.id.timePicker_alto);
-        editText_time.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                calendar_1 = Calendar.getInstance();
-                currentHour = calendar_1.get(Calendar.HOUR_OF_DAY);
-                currentMinute = calendar_1.get(Calendar.MINUTE);
-
-                timePickerDialog = new TimePickerDialog(BookNowAltoK10.this, new OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
-                        if (hourOfDay >= 12){
-
-                            amPm = "PM";
-                        } else {
-
-                            amPm = "AM";
-                        }
-
-                        editText_time.setText(String.format("%02d:%02d", hourOfDay, minute) + amPm);
-    public void openSuccess(){
-
-        Intent intent = new Intent(this,Success.class);
-        startActivity(intent);
-    }
-
-
-                    }
-                },currentHour,currentMinute,false);
     public void SendInput(){
 
-                timePickerDialog.show();
-            }
-        });
-
-        userLocation_1 = findViewById(R.id.alto_your_location);
-        userpickupLocation = findViewById(R.id.alto_pickup_location);
-        user = findViewById(R.id.username_bookAlto);
-        email = findViewById(R.id.email_bookAlto);
-        phone = findViewById(R.id.phone_bookAlto);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseDatabase = FirebaseDatabase.getInstance();
         YourLocation = userLocation_1.getText().toString();
         PickUpLocation = userpickupLocation.getText().toString();
         Date = editText_date.getText().toString();
         Time = editText_time.getText().toString();
 
-        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid()).child("User Info");
+        DatabaseReference myref1 = firebaseDatabase.getReference(firebaseAuth.getUid()).child("AltoK10")
+                .child("Booking Information").child("UserLocation");
+        DatabaseReference myref2 = firebaseDatabase.getReference(firebaseAuth.getUid()).child("AltoK10")
+                .child("Booking Information").child("PickUpLocation");
+        DatabaseReference myref3 = firebaseDatabase.getReference(firebaseAuth.getUid()).child("AltoK10")
+                .child("Booking Information").child("Date");
+        DatabaseReference myref4 = firebaseDatabase.getReference(firebaseAuth.getUid()).child("AltoK10")
+                .child("Booking Information").child("Time");
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        myref1.setValue(YourLocation);
+        myref2.setValue(PickUpLocation);
+        myref3.setValue(Date);
+        myref4.setValue(Time);
+    }
+
+    private void openDialog() {
+
+        final AlertDialog.Builder dialog = new Builder(BookNowAltoK10.this);
+        dialog.setTitle("Booking Information");
+        LayoutInflater inflater = LayoutInflater.from(BookNowAltoK10.this);
+        View book_layout = inflater.inflate(R.layout.layout_dialog, null);
+
+        tripLocation = book_layout.findViewById(R.id.trip_info_location);
+        tripPickup = book_layout.findViewById(R.id.trip_info_pickup);
+        tripDate = book_layout.findViewById(R.id.trip_info_date);
+        tripTime = book_layout.findViewById(R.id.trip_info_time);
+        tripBook = book_layout.findViewById(R.id.trip_info_book);
+        tripExtra = book_layout.findViewById(R.id.trip_info_extra);
+
+        dialog.setView(book_layout);
+
+        final DatabaseReference databaseReference1 = firebaseDatabase.getReference(firebaseAuth.getUid())
+                .child("AltoK10").child("Booking Information");
+        databaseReference1.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                String UserName = dataSnapshot.child("UserName").getValue().toString();
-                String UserEmail = dataSnapshot.child("UserEmail").getValue().toString();
-                String UserPhone = dataSnapshot.child(("UserPhone")).getValue().toString();
-                user.setText(UserName);
-                email.setText(UserEmail);
-                phone.setText(UserPhone);
+                String location = dataSnapshot.child("UserLocation").getValue().toString();
+                String pickUp = dataSnapshot.child("PickUpLocation").getValue().toString();
+                String date = dataSnapshot.child("Date").getValue().toString();
+                String time = dataSnapshot.child("Time").getValue().toString();
+                String book = dataSnapshot.child("Booking Price").getValue().toString();
+                String extra = dataSnapshot.child("Rental Extra Options").getValue().toString();
+
+                tripLocation.setText(location);
+                tripPickup.setText(pickUp);
+                tripDate.setText(date);
+                tripTime.setText(time);
+                tripBook.setText(book);
+                tripExtra.setText(extra);
+
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                Toast.makeText(BookNowAltoK10.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                final DatabaseReference databaseReference2 = firebaseDatabase.getReference(firebaseAuth.getUid())
+                        .child("AltoK10").child("Booking Information");
+                databaseReference2.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        databaseReference2.child("UserLocation").setValue(tripLocation.getText().toString());
+                        databaseReference2.child("PickUpLocation").setValue(tripPickup.getText().toString());
+                        databaseReference2.child("Date").setValue(tripDate.getText().toString());
+                        databaseReference2.child("Time").setValue(tripTime.getText().toString());
+                        databaseReference2.child("Booking Price").setValue(tripBook.getText().toString());
+                        databaseReference2.child("Rental Extra Options").setValue(tripExtra.getText().toString());
+
+                        Intent intent = new Intent(BookNowAltoK10.this, PaymentSelection.class);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
 
             }
         });
+
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
-
-    public void SendInput(){
-
-        YourLocation = userLocation_1.getText().toString();
-        PickUpLocation = userpickupLocation.getText().toString();
-        Date = editText_date.getText().toString();
-        Time = editText_time.getText().toString();
-
-        DatabaseReference myref = firebaseDatabase.getReference(firebaseAuth.getUid()).child("AltoK10")
-                .child("Booking Information");
-        BookingProfile bookingProfile = new BookingProfile(YourLocation,PickUpLocation,Date,Time);
-        myref.setValue(bookingProfile);
-        DatabaseReference myref = firebaseDatabase.getReference(firebaseAuth.getUid()).child("AltoK10")
-                .child("Booking Information");
-        BookingProfile bookingProfile = new BookingProfile(YourLocation,PickUpLocation,Date,Time);
-        myref.setValue(bookingProfile);
-    }
-
-    //gone
 }
